2368b021f32fde35da9a2451354a3e618d424124
Kiran Tirmale <127860308+kirantirmale@users.noreply.github.com>
2025-02-09 10:14:30 +0530

    main update

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -30,11 +30,22 @@ import whatsapp from "../images/features/whatsapp.png";
 import other from "../images/features/search.png";
 import { SlArrowUpCircle } from "react-icons/sl";
 
-const Features = ({ setActiveSection }) => {
+const Features = () => {
 
   const [showTopBtn, setShowTopBtn] = useState(false);
 
- 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 300) {
+        setShowTopBtn(true);
+      } else {
+        setShowTopBtn(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
